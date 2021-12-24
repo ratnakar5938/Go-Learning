dd933281a46d03c0c0285d575d760e1d35affdd2
Ratnakar <ratnakar3895@yahoo.com>
2021-12-24 18:50:52 +0530

    Consuming JSON in GO

--- 21bitmorejson/main.go
+++ 21bitmorejson/main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("More on JSON in GO")
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -31,4 +32,34 @@ func EncodeJson() {
 		panic(err)
 	}
 	fmt.Printf("%s\n", finalJson)
+}
+
+func DecodeJson() {
+	jsonData := []byte(`
+	{
+		"coursename": "ReactJs Bootcamp",
+		"Price": 299,
+		"website": "MyPlatform.com",
+		"tags": ["web-dev", "js"]
+	}
+	`)
+
+	var myCourse course
+	checkValid := json.Valid(jsonData)
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonData, &myCourse)
+		fmt.Printf("%#v\n", myCourse)
+	} else {
+		fmt.Println("JSON was not valid")
+	}
+
+	// some cases where you just want to add data to key value
+	var myData map[string]interface{}
+	json.Unmarshal(jsonData, &myData)
+	fmt.Printf("%#v\n", myData)
+
+	for key, value := range myData {
+		fmt.Printf("Key is %v, Value is %v and Type is %T\n", key, value, value)
+	}
 }
