a792e5fe3d400fa20df16dc4f0e1908e593cfebe
Ratnakar <ratnakar3895@yahoo.com>
2021-12-25 21:56:45 +0530

    Building API in GO

---
+++ 23buildapi/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// model for courses - file
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	FullName string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake DB
+var courses []Course
+
+// middleware, helper - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+	fmt.Println("Building API in GO")
+}
