58c062e534ffbbbe73074545f7e4fb92449136f3
Ratnakar <ratnakar3895@yahoo.com>
2021-12-26 19:06:36 +0530

    Handling routes and testing routes in GO

--- 23buildapi/main.go
+++ 23buildapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -34,7 +35,24 @@ func (c *Course) IsEmpty() bool {
 }
 
 func main() {
-	fmt.Println("Building API in GO")
+	fmt.Println("API in GO")
+	r := mux.NewRouter()
+
+	// seeding
+	courses = append(courses, Course{CourseId: "2", CourseName: "ReactJS", CoursePrice: 399, Author: &Author{FullName: "Ratnakar", Website: "ratnakar5938.dev.go"}})
+	courses = append(courses, Course{CourseId: "4", CourseName: "MERN", CoursePrice: 199, Author: &Author{FullName: "Ratnakar", Website: "ratnakar5938.dev.go"}})
+
+	// routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+	r.HandleFunc("/courses", deleteAllCourse).Methods("DELETE")
+
+	// listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 // controllers - file
@@ -47,6 +65,10 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
+	if courses == nil {
+		json.NewEncoder(w).Encode("No courses available")
+		return
+	}
 	json.NewEncoder(w).Encode(courses)
 }
 
@@ -67,7 +89,6 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	json.NewEncoder(w).Encode("No course found with given id")
-	return
 }
 
 func createOneCourse(w http.ResponseWriter, r *http.Request) {
@@ -88,13 +109,21 @@ func createOneCourse(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// check only if title is duplicate
+	// loop, title matches course.CourseName
+	for _, each_course := range courses {
+		if each_course.CourseName == course.CourseName {
+			json.NewEncoder(w).Encode("Duplicated Title...Please use another title")
+			return
+		}
+	}
+
 	// generate unique ID, string
 	// append new course into courses
 	rand.Seed(time.Now().UnixNano())
 	course.CourseId = strconv.Itoa(rand.Intn(100))
 	courses = append(courses, course)
 	json.NewEncoder(w).Encode(course)
-	return
 }
 
 func updateOneCourse(w http.ResponseWriter, r *http.Request) {
@@ -110,7 +139,7 @@ func updateOneCourse(w http.ResponseWriter, r *http.Request) {
 			courses = append(courses[:index], courses[index+1:]...)
 			var given_course Course
 			_ = json.NewDecoder(r.Body).Decode(&given_course)
-			course.CourseId = params["id"]
+			given_course.CourseId = params["id"]
 			courses = append(courses, given_course)
 			json.NewEncoder(w).Encode(given_course)
 			return
@@ -132,7 +161,18 @@ func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
 		if course.CourseId == params["id"] {
 			courses = append(courses[:index], courses[index+1:]...)
 			json.NewEncoder(w).Encode("Course deleted")
-			break
+			return
 		}
 	}
+
+	json.NewEncoder(w).Encode("Course not found")
+}
+
+func deleteAllCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete all course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// clear all values from courses
+	courses = nil
+	json.NewEncoder(w).Encode("All courses deleted")
 }
